345717a234e63ea468e5252f678342167a8508b0
mpaulosky <matthew.paulosky@outlook.com>
2022-03-18 10:12:16 -0700

    Add Copy Of PowerShell 7 Profile

---
+++ PowerShell 7/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+
+#Import-Module posh-git
+#Import-Module oh-my-posh
+#Import-Module -Name Terminal-Icons
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+#Set-PoshPrompt -Theme D:\OneDrive\Documents\posh-git-themes\ohmyposhv3.json
+Set-PoshPrompt -Theme D:\OneDrive\Documents\posh-git-themes\fritz-mytheme.omp.json
+#Set-PoshPrompt -Theme iterm2
+
